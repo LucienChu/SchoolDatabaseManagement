526e5f92f2e796497b0276cd152ab2500d40fd68
LucienChu <lamborkingly@gmail.com>
2018-06-05 23:49:07 -0400

    almost done

--- src/admin/AdminController.java
+++ src/admin/AdminController.java
@@ -1,4 +1,114 @@
 package admin;
 
-public class AdminController {
+import dbUtil.dbConnection;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
+
+public class AdminController implements Initializable {
+    @FXML
+    private TextField id;
+    @FXML
+    private TextField firstName;
+    @FXML
+    private TextField lastName;
+    @FXML
+    private TextField emailAddress;
+    @FXML
+    private DatePicker dateOfBirth;
+    @FXML
+    private TableView<StudentData> studentTable = new TableView<StudentData>();
+    @FXML
+    private TableColumn<StudentData, String> idColumn;
+    @FXML
+    private TableColumn<StudentData, String> firstNameColumn;
+    @FXML
+    private TableColumn<StudentData, String> lastNameColumn;
+    @FXML
+    private TableColumn<StudentData, String> emailColumn;
+    @FXML
+    private TableColumn<StudentData, String> dateOfBirthColumn;
+    private ObservableList<StudentData> studentData = null;
+    private dbConnection dbc;
+
+    private String sql = "SELECT * FROM students";
+
+    public void initialize(URL url, ResourceBundle rb){
+        this.dbc = new dbConnection();
+    }
+
+    @FXML
+    private void LoadStudnetData(ActionEvent event){
+        try{
+            Connection connect = dbConnection.getConnection();
+            this.studentData = FXCollections.observableArrayList();
+
+            ResultSet rs = connect.createStatement().executeQuery(sql);
+            while (rs.next()){
+                this.studentData.add(new StudentData(
+                        rs.getString(1),
+                        rs.getString(2),
+                        rs.getString(3),
+                        rs.getString(4),
+                        rs.getString(5)));
+            }
+        }catch (SQLException e){
+            System.err.println("Error " + e);
+        }
+
+        // display data on the table
+        this.idColumn.setCellValueFactory(new PropertyValueFactory<StudentData, String>("ID"));
+        this.firstNameColumn.setCellValueFactory(new PropertyValueFactory<StudentData, String>("firstName"));
+        this.lastNameColumn.setCellValueFactory(new PropertyValueFactory<StudentData, String>("lastName"));
+        this.emailColumn.setCellValueFactory(new PropertyValueFactory<StudentData, String>("email"));
+        this.dateOfBirthColumn.setCellValueFactory(new PropertyValueFactory<StudentData, String>("dateOfBirth"));
+
+        this.studentTable.setItems(null);
+        this.studentTable.setItems(studentData);
+    }
+
+    //add data to database
+    @FXML
+    private void addStudent(ActionEvent event){
+        String insertSQL = "INSERT INTO students(id, firstNamt, lastName, email, dateOfBirth) VALUES(?,?,?,?)";
+        try{
+            Connection connect = new dbConnection().getConnection();
+            PreparedStatement ps = connect.prepareStatement(insertSQL);
+
+            ps.setString(1, this.id.getText());
+            ps.setString(2, this.firstName.getText());
+            ps.setString(3, this.lastName.getText());
+            ps.setString(4, this.emailAddress.getText());
+            ps.setString(5, this.dateOfBirth.getEditor().getText());
+
+            ps.execute();
+            ps.close();
+        }catch(SQLException e){
+            System.err.print("Error " + e);
+        }
+    }
+
+    //clear form after adding student
+    @FXML
+    private void clearFields(ActionEvent event){
+        this.id.setText("");
+        this.firstName.setText("");
+        this.lastName.setText("");
+        this.emailAddress.setText("");
+        this.dateOfBirth.setValue(null);
+    }
 }
--- src/admin/StudentData.java
+++ src/admin/StudentData.java
@@ -1,4 +1,80 @@
 package admin;
 
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
 public class StudentData {
+    private final StringProperty ID;
+    private final StringProperty firstName;
+    private final StringProperty lastName;
+    private final StringProperty email;
+    private final StringProperty dateOfBirth;
+
+    public StudentData(String id, String firstName, String lastName, String email, String birthDate){
+        this.ID = new SimpleStringProperty(id); //read the documentation
+        this.firstName = new SimpleStringProperty(firstName);
+        this.lastName = new SimpleStringProperty(lastName);
+        this.email = new SimpleStringProperty(email);
+        this.dateOfBirth = new SimpleStringProperty(birthDate);
+    }
+
+    public String getID() {
+        return ID.get();
+    }
+
+    public StringProperty IDProperty() {
+        return ID;
+    }
+
+    public String getFirstName() {
+        return firstName.get();
+    }
+
+    public StringProperty firstNameProperty() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName.get();
+    }
+
+    public StringProperty lastNameProperty() {
+        return lastName;
+    }
+
+    public String getEmail() {
+        return email.get();
+    }
+
+    public StringProperty emailProperty() {
+        return email;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth.get();
+    }
+
+    public StringProperty dateOfBirthProperty() {
+        return dateOfBirth;
+    }
+
+    public void setID(String ID) {
+        this.ID.set(ID);
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName.set(firstName);
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName.set(lastName);
+    }
+
+    public void setEmail(String email) {
+        this.email.set(email);
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth.set(dateOfBirth);
+    }
 }
