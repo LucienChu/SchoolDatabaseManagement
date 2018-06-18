d7fe3f1b623d7ac38ce66531763441f25887e7a9
LucienChu <lamborkingly@gmail.com>
2018-06-18 14:29:19 -0400

    update function is done, working on functional optimization

--- src/admin/ModifyDataController.java
+++ src/admin/ModifyDataController.java
@@ -4,10 +4,16 @@ import dbUtil.dbConnection;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
 
 public class ModifyDataController implements Initializable {
@@ -15,6 +21,7 @@ public class ModifyDataController implements Initializable {
 
     @FXML private TextField id,firstName, lastName, email;
     @FXML private DatePicker dateOfBirth;
+    @FXML private Label label;
     public void initialize(URL url, ResourceBundle rb){
         this.dbc = new dbConnection();
     }
@@ -36,4 +43,36 @@ public class ModifyDataController implements Initializable {
         this.dateOfBirth.setValue(null);
     }
 
+    @FXML
+    private void updateData(ActionEvent event){
+        String updateSQL = "UPDATE students SET lName = ?, fName = ?, email = ?, DOB = ? WHERE id = ?";
+        try{
+            Connection connect = new dbConnection().getConnection();
+            PreparedStatement ps = connect.prepareStatement(updateSQL);
+
+            ps.setString(1, this.firstName.getText());
+            ps.setString(2, this.lastName.getText());
+            ps.setString(3, this.email.getText());
+            ps.setString(4, this.dateOfBirth.getEditor().getText());
+            ps.setString(5, this.id.getText());
+
+            ps.execute();
+            ps.close();
+
+            this.label.setText("Update is done!");
+
+
+        }catch (SQLException e){
+            this.label.setText("Error detected, please try again!");
+        }
+    }
+    @FXML
+    private Button cancelBtn;
+
+    @FXML
+    private void closeWindow(ActionEvent event){
+        Stage stage = (Stage)cancelBtn.getScene().getWindow();
+        stage.close();
+
+    }
 }
