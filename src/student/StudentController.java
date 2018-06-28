8e95f830c3c515513162479b6e330d0b98f32836
LucienChu <lamborkingly@gmail.com>
2018-06-27 21:50:27 -0400

    EVERYTHING IS DONE, CONGRAT

--- src/student/StudentController.java
+++ src/student/StudentController.java
@@ -2,10 +2,12 @@ package student;
 
 import admin.StudentData;
 import dbUtil.dbConnection;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.Pane;
@@ -13,6 +15,10 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
 
 import static java.lang.Double.parseDouble;
@@ -23,18 +29,11 @@ public class StudentController implements Initializable {
     private StudentData studentData = null;
     private dbConnection dbc = null;
     public void initialize(URL url, ResourceBundle rb){}
+    @FXML
+    private Button cancelBtn;
 
     public void setStudentData(StudentData studentData) {
         this.studentData = studentData;
-    }
-    public void setCurrentStudent (StudentData studentData){
-        this.studentData = new StudentData(
-                studentData.getID(),
-                studentData.getFirstName(),
-                studentData.getLastName(),
-                studentData.getEmail(),
-                studentData.getDateOfBirth(),
-                parseDouble(studentData.getBalance()));
     }
 
     public void initializeStudentData(){
@@ -62,9 +61,43 @@ public class StudentController implements Initializable {
             Pane root = loader.load(getClass().getResource("/student/StudentPay.fxml").openStream());
             Scene scene = new Scene(root);
             stage.setScene(scene);
+            StudentPayController studentPayController = loader.getController();
+            studentPayController.setStudentData(this.studentData);
             stage.show();
         }catch (IOException e){
             e.getStackTrace();
         }
+    }
+
+    @FXML
+    public void refresh(){
+        try{
+            Connection connection = new dbConnection().getConnection();
+            String sql = "SELECT * FROM students WHERE id = " + this.studentData.getID();
+
+            ResultSet rs = connection.createStatement().executeQuery(sql);
+            if (rs != null){
+                StudentData aStudentData = new StudentData(
+                        rs.getString(1),
+                        rs.getString(2),
+                        rs.getString(3),
+                        rs.getString(4),
+                        rs.getString(5),
+                        rs.getDouble(6));
+                this.studentData = aStudentData;
+            }
+            rs.close();
+            connection.close();
+            this.initializeStudentData();
+            }catch (SQLException e){
+            e.getStackTrace();
+        }
+    }
+
+    @FXML
+    private void closeWindow(ActionEvent event){
+        Stage stage = (Stage)cancelBtn.getScene().getWindow();
+        stage.close();
+
     }
 }
--- src/student/StudentPayController.java
+++ src/student/StudentPayController.java
@@ -1,11 +1,72 @@
 package student;
 
+import admin.StudentData;
+import dbUtil.dbConnection;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 import java.awt.geom.RectangularShape;
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
 
+import static java.lang.Double.parseDouble;
+
 public class StudentPayController implements Initializable {
+    private dbConnection connect;
+    private StudentData studentData = null;
+
+    @FXML
+    private TextField inputAmount;
+    @FXML
+    private Label paymentLabel;
+    @FXML
+    private Button cancelBtn;
     public void initialize(URL url, ResourceBundle rb){}
+
+    public void addPayment(ActionEvent event){
+        String intRegex = "[0-9]*";
+        String doubleRegex = "[0-9]*\\.[0-9]{1,2}";
+        String paymentString = this.inputAmount.getText();
+        if((paymentString.matches(intRegex) ||
+            paymentString.matches(doubleRegex))
+            && !paymentString.contains("-")) {
+            try {
+                Connection connection = new dbConnection().getConnection();
+                String updateSQL = "UPDATE students SET balance = balance - ? WHERE id = ?";
+                PreparedStatement ps = connection.prepareStatement(updateSQL);
+                ps.setString(1, paymentString);
+                ps.setString(2, this.studentData.getID());
+
+                ps.executeUpdate();
+                ps.close();
+                connection.close();
+                this.paymentLabel.setText("Payment accepted!");
+
+            } catch (SQLException e) {
+                this.paymentLabel.setText("SQL exception detected.");
+            }
+        }
+        else{
+               this.paymentLabel.setText("Invalid input");
+            }
+        }
+
+    public void setStudentData(StudentData studentData) {
+        this.studentData = studentData;
+    }
+
+    @FXML
+    private void closeWindow(ActionEvent event){
+        Stage stage = (Stage)cancelBtn.getScene().getWindow();
+        stage.close();
+
+    }
 }
